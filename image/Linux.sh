9c110e3d96847c4b848b44161dbf1fb99aabeb9c
takeshi <none@none>
2009-03-04 02:00:32 +0000

    Budowanie w systemie Linux

--- image/Linux.sh
+++ image/Linux.sh
@@ -8,8 +8,6 @@ fi
 set -x
 
 mount -o loop floppy.img fd
-mv root/boot/impala root/boot/impala.old
-cp ../kernel/impala root/boot/impala
 cp root/boot/impala* fd/boot/
 cp root/boot/grub/menu.lst fd/boot/grub/
 sync
