179c19ba4e7c835fc3b527aabcc53e22f49c34bb
takeshi <none@none>
2009-06-21 18:37:39 +0000

    Tymczasowy getmountinfo.c, aby się budowało

---
+++ sys/kern/sc/getmountinfo.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2009 University of Wroclaw. Department of Computer Science
+ *    http://www.ii.uni.wroc.pl/
+ * Copyright (C) 2009 Mateusz Kocielski, Artur Koninski, Pawel Wieczorek
+ *    http://trzask.codepainters.com/impala/trac/
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: TEMPLATE.c 457 2009-06-21 14:41:46Z wieczyk $
+ */
+
+#include <sys/types.h>
+#include <sys/kernel.h>
+#include <sys/syscall.h>
+#include <sys/vm.h>
+
+typedef struct getmountinfo_args getmountinfo_args_t;
+struct getmountinfo_args {
+    int fd;
+};
+
+int sc_getmountinfo(thread_t *p, syscall_result_t *r, getmountinfo_args_t *args);
+
+int
+sc_getmountinfo(thread_t *t, syscall_result_t *r, getmountinfo_args_t *args)
+{
+    return 0;
+}
+
+
