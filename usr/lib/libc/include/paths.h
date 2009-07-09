93ea4ea5ec82cbb5503c94cee72e3c50c9f00573
takeshi <none@none>
2009-07-09 10:17:32 +0000

    Licencja w nagłówkach

--- usr/lib/libc/include/paths.h
+++ usr/lib/libc/include/paths.h
@@ -1,5 +1,19 @@
+/*
+ * System operacyjny Impala.
+ *
+ * Copyright (C) 2009 University of Wroclaw. Department of Computer Science
+ *    http://www.ii.uni.wroc.pl/
+ * Copyright (C) 2009 Mateusz Kocielski, Artur Koninski, Pawel Wieczorek
+ *    http://trzask.codepainters.com/impala/trac/
+ * All rights reserved.
+ *
+ * Niniejszy plik jest objêty licencj±, zobacz plik COPYRIGHT dostarczony
+ * wraz z projektem.
+ *
+ * $Id$
+ */ 
 #ifndef __PATHS_H
-#define __PATHS_S
+#define __PATHS_H
 
 #define _PATH_DEVNULL   "/dev/null"
 #define _PATH_DEFPATH   "/usr/bin:/bin"
--- usr/lib/libc/sys/getprocinfo.c
+++ usr/lib/libc/sys/getprocinfo.c
@@ -1,3 +1,17 @@
+/*
+ * System operacyjny Impala.
+ *
+ * Copyright (C) 2009 University of Wroclaw. Department of Computer Science
+ *    http://www.ii.uni.wroc.pl/
+ * Copyright (C) 2009 Mateusz Kocielski, Artur Koninski, Pawel Wieczorek
+ *    http://trzask.codepainters.com/impala/trac/
+ * All rights reserved.
+ *
+ * Niniejszy plik jest objêty licencj±, zobacz plik COPYRIGHT dostarczony
+ * wraz z projektem.
+ *
+ * $Id$
+ */ 
 #include <sys/types.h>
 #include <sys/proc.h>
 #include <sys/syscall.h>
