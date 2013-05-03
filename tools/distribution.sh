7626f44ae78709b3568ab4744d08e2a4e1b08e36
Pawel Wieczorek <wieczyk@gmail.com>
2013-05-03 19:04:31 +0200

    Good distribution.sh

--- tools/distribution.sh
+++ tools/distribution.sh
@@ -1,8 +1,17 @@
 
-create_directory() {
-    rm -rf distribution/${DIST_PROFILE}
-    mkdir -p distribution/${DIST_PROFILE}/sysroot
-    mkdir -p distribution/${DIST_PROFILE}/image
+prepare_dirs() {
+    WORKDIR=distribution/${DIST_PROFILE}
+    SYSPACK_DIR=${WORKDIR}/syspack
+    IMAGE_DIR=${WORKDIR}/image
+    rm -rf ${WORKDIR}
+    mkdir -p ${SYSPACK_DIR}
+    mkdir -p ${SYSPACK_DIR}/bin
+    mkdir -p ${SYSPACK_DIR}/sbin
+    mkdir -p ${SYSPACK_DIR}/demos
+    mkdir -p ${IMAGE_DIR}
+    mkdir -p ${IMAGE_DIR}/boot
+    mkdir -p ${IMAGE_DIR}/boot/grub
+    mkdir -p ${IMAGE_DIR}/impala
 }
 
 check_profile() {
@@ -12,9 +21,45 @@ check_profile() {
     fi
 }
 
+prepare_root() {
+    cp image/_floppy.img ${WORKDIR}/floppy.img
+    cp COPYRIGHT ${IMAGE_DIR}
+    cp sys/kern/impala.gz ${IMAGE_DIR}/boot/
+    cp image/root/boot/grub/menu.lst ${IMAGE_DIR}/boot/grub
+    mv ${WORKDIR}/syspack.tar.gz ${IMAGE_DIR}/impala/
+    cp usr/sbin/preinit/preinit ${IMAGE_DIR}/impala/
+    cp usr/bin/tar/tar ${IMAGE_DIR}/impala/
+    cp usr/bin/minigzip/minigzip ${IMAGE_DIR}/impala/
+
+}
+
+prepare_syspack() {
+    cp -r usr/etc ${SYSPACK_DIR}
+    for file in `cat ${PROFILE_FILE}`; do
+         exe_name=`basename ${file}`
+         cp ./usr/${file}/${exe_name} ${SYSPACK_DIR}/${file}
+    done
+    (cd ${SYSPACK_DIR}; tar --format=ustar -zcf ../syspack.tar.gz .)
+}
+
+build_image() {
+    mcopy -s -i ../floppy.img impala ::/impala
+    mcopy -s -i ../floppy.img boot/impala.gz ::/boot/
+    mcopy -s -i ../floppy.img boot/grub/menu.lst ::/boot/grub/
+    for f in `find . -maxdepth 1 -type f`; do
+        if [ $f != "." ]; then
+            mcopy -i ../floppy.img $f ::$f
+        fi
+    done
+}
+
 handle_profile() {
     echo "====> Building floppy image for profile ${DIST_PROFILE}"
 
+    prepare_dirs
+    prepare_syspack
+    prepare_root
+    (cd ${IMAGE_DIR}; build_image)
 }
 
 if [ -z ${DIST_PROFILE} ]; then
