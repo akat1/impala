b4f988d44334f34019be55031ed3ed72a91a4e8b
Mateusz Kocielski <shm@digitalsun.pl>
2017-03-09 13:11:54 +0100

    Welcome PATA PIO driver
    For now it supports only read operation, but it's enough to get mfs
    form the hdd. PIO is horribly slow, so treat it as a temporary solution.

--- sys/dev/pata/pata.c
+++ sys/dev/pata/pata.c
@@ -30,7 +30,28 @@
  * $Id$
  */
 
+#include <sys/types.h>
+#include <sys/kernel.h>
+#include <sys/device.h>
+#include <sys/bio.h>
 #include <dev/pata/pata.h>
+#include <fs/devfs/devfs.h>
+#include <machine/interrupt.h>
+#include <machine/io.h>
+#include <machine/bus/pci.h>
+
+/* For now we implement only PIO mode.
+ * The good news is that every ATA device must implement it (in order to
+ * confirm the standard)
+ * XXX: The bad news is it's horrible slow, who cares for now!
+ *
+ * TODO:
+ *
+ * - major clean up
+ * - add write
+ * - physio
+ * - get rid of PIO
+ */
 
 /*
  * I'm not sure if pata is right name.
@@ -39,3 +60,245 @@
  * http://suif.stanford.edu/~csapuntz/specs/idems100.ps
  */
 
+/* no more devices than 4: 2 x PRIMARY + 2 x SECONDARY */
+#define PATA_DEVICES_MAX    (4)
+
+struct pata_device {
+    uint16_t bus;   /* PRIMARY or SECONDARY */
+    uint8_t disk;  /* MASTER or SLAVE */
+    const char *name;
+    devd_t  *dev;   /* Associated device */
+    uint8_t busy;   /* am i busy? */
+    /* ... */
+} pata_devices[PATA_DEVICES_MAX];
+
+int pata_devices_no = 0;
+
+/* private */
+void _pata_set_drive(int bus, int disk);
+void _pata_send_command(int bus, int cmd);
+uint8_t _pata_read_status(int bus);
+void _pata_probe(int bus, int disk);
+void _pata_register(int bus, int disk, uint16_t *identify);
+size_t _pata_read_lba28(struct pata_device *dev, uint16_t *buf, uint32_t addr,
+  uint8_t count);
+int pata_open(devd_t *d, int flags);
+int pata_close(devd_t *d);
+int pata_strategy(devd_t *d, iobuf_t *bp);
+
+devsw_t pata_devsw = {
+    pata_open,
+    pata_close,
+    noioctl,
+    noread,
+    nowrite,
+    pata_strategy,
+    DEV_BDEV
+};
+
+
+/*
+ * Sets current disk on particular bus
+ * @bus - primary/secondary
+ * @disk - master/slave
+ */
+void
+_pata_set_drive(int bus, int disk)
+{
+    io_out8(PATA_REGISTER(bus, PATA_REG_DRIVE), disk);
+}
+
+/*
+ _pata_probe
+ _pata_register
+*/
+
+void
+_pata_send_command(int bus, int cmd)
+{
+    io_out8(PATA_REGISTER(bus, PATA_REG_COMMAND), cmd);
+}
+
+
+uint8_t
+_pata_read_status(int bus)
+{
+    return io_in8(PATA_REGISTER(bus, PATA_REG_STATUS));
+}
+
+
+/* read sector */
+size_t
+_pata_read_lba28(struct pata_device *dev, uint16_t *buf, uint32_t addr,
+        uint8_t count)
+{
+    uint8_t drv, status, sect;
+    int i;
+
+    /* XXX: move it to set_drive */
+    drv = (dev->disk == PATA_MASTER) ? 0xE0 : 0xF0;
+    drv |= (addr >> 24) & 0x0F;
+
+    io_out8(PATA_REGISTER(dev->bus, PATA_REG_DRIVE), drv);
+    io_out8(PATA_REGISTER(dev->bus, PATA_REG_FEATURES), 0);
+    io_out8(PATA_REGISTER(dev->bus, PATA_REG_SECTOR_CNT), count);
+
+    /* lba28 */
+    io_out8(PATA_REGISTER(dev->bus, PATA_REG_LBA_LO), addr & 0xFF);
+    io_out8(PATA_REGISTER(dev->bus, PATA_REG_LBA_MID), (addr >> 8) & 0xFF);
+    io_out8(PATA_REGISTER(dev->bus, PATA_REG_LBA_HI), (addr >> 16) & 0xFF);
+
+    _pata_send_command(dev->bus, PATA_CMD_PIO_READ);
+
+    for (sect = 0; sect < count; sect++) {
+        /* 400ns delay (reading status takes 100ns) */
+        for (i = 0; i < 4; i++) {
+            (void)_pata_read_status(dev->bus);
+        }
+
+        /* XXX: irq some day */
+        for(;;) {
+            status = _pata_read_status(dev->bus);
+            if ((status & PATA_STATUS_ERR) != 0)
+                return -1;
+            if ((status & PATA_STATUS_DF) != 0)
+                return -1;
+            if ((status & PATA_STATUS_BSY) == 0 &&
+             (status & PATA_STATUS_DRQ) != 0)
+                break;
+        }
+
+
+        for (i = 0; i < 256; i++) {
+            /* XXX: repinsw? */
+            buf[sect*256+i] = io_in16(PATA_REGISTER(dev->bus, PATA_REG_DATA));
+        }
+    }
+
+    return count;
+}
+
+void
+_pata_register(int bus, int disk, uint16_t *identify)
+{
+    pata_devices[pata_devices_no].bus = bus;
+    pata_devices[pata_devices_no].disk = disk;
+
+    /* XXX: deuglify me */
+    if (bus == PATA_PRIMARY) {
+        if (disk == PATA_MASTER)
+            pata_devices[pata_devices_no].name = "ata0d0";
+        else
+            pata_devices[pata_devices_no].name = "ata0d1";
+    } else {
+        if (disk == PATA_MASTER)
+            pata_devices[pata_devices_no].name = "ata1d0";
+        else
+            pata_devices[pata_devices_no].name = "ata1d1";
+    }
+
+    pata_devices[pata_devices_no].dev = devd_create(&pata_devsw,
+     pata_devices[pata_devices_no].name, -1, &pata_devices[pata_devices_no]);
+    devfs_register(pata_devices[pata_devices_no].dev, 0, 0, 0600);
+
+    pata_devices_no++;
+}
+
+void
+_pata_probe(int bus, int disk)
+{
+    int i;
+    uint16_t identify[256];
+
+    /* select drive */
+    _pata_set_drive(bus, disk);
+
+    /* set 0s */
+    io_out8(PATA_REGISTER(bus, PATA_REG_SECTOR_CNT), 0);
+    io_out8(PATA_REGISTER(bus, PATA_REG_LBA_LO), 0);
+    io_out8(PATA_REGISTER(bus, PATA_REG_LBA_MID), 0);
+    io_out8(PATA_REGISTER(bus, PATA_REG_LBA_HI), 0);
+
+    /* send IDENTIFY */
+    _pata_send_command(bus, PATA_CMD_IDENTIFY);
+
+    /* read status */
+    if (_pata_read_status(bus) == 0) {
+        /* drive does not exists */
+        return;
+    }
+
+    /* wait until ready */
+    while (_pata_read_status(bus) & PATA_STATUS_BSY);
+
+    /* could be ATAPI drive, according to:
+     * http://wiki.osdev.org/ATA_PIO_Mode#IDENTIFY_command */
+    if (io_in8(PATA_REGISTER(bus, PATA_REG_LBA_MID)) != 0 ||
+      io_in8(PATA_REGISTER(bus, PATA_REG_LBA_HI)) != 0) {
+        /* found ATAPI device, run away */
+        return;
+    }
+
+    /* wait until data is ready */
+    while ((_pata_read_status(bus) & (PATA_STATUS_DRQ | PATA_STATUS_ERR))
+      == 0);
+
+    if (_pata_read_status(bus) & PATA_STATUS_ERR) {
+        /* error has occured, screw that drive */
+        return;
+    }
+
+    /* read data */
+    for (i = 0 ; i < 256; i++)
+        identify[i] = io_in16(PATA_REGISTER(bus, PATA_REG_DATA));
+
+    /* register device */
+    _pata_register(bus, disk, identify);
+}
+
+void
+pata_init(void)
+{
+    _pata_probe(PATA_PRIMARY, PATA_MASTER);
+    _pata_probe(PATA_PRIMARY, PATA_SLAVE);
+    _pata_probe(PATA_SECONDARY, PATA_MASTER);
+    _pata_probe(PATA_SECONDARY, PATA_SLAVE);
+
+}
+
+int
+pata_open(devd_t *d, int flags)
+{
+    struct pata_device *dev = d->priv;
+    if (dev->busy != FALSE) {
+        return -EBUSY;
+    }
+    dev->busy = TRUE;
+    return 0;
+}
+
+int
+pata_close(devd_t *d)
+{
+    struct pata_device *dev = d->priv;
+    dev->busy = FALSE;
+    return 0;
+}
+
+int
+pata_strategy(devd_t *d, iobuf_t *b)
+{
+    int s;
+
+    struct pata_device *dev = d->priv;
+    s = splbio();
+    if (b->oper) {
+        /* XXX: bcount is bounded here */
+        _pata_read_lba28(dev, b->addr, b->blkno, b->bcount);
+    } else
+        /* not supported for now */
+        b->flags |= BIO_ERROR;
+    splx(s);
+    bio_done(b);
+    return 0;
+}
--- sys/dev/pata/pata.h
+++ sys/dev/pata/pata.h
@@ -34,18 +34,81 @@
 #define __DEV_PATA_PATA_H
 #ifdef __KERNEL
 
+void pata_init(void);
+
+/* XXX:
+   screw CHS
+   LBA28
+   LBA48
+*/
+
+/* 
+ * http://ethv.net/workshops/osdev/notes/notes-5 
+ * http://wiki.osdev.org/ATA_PIO_Mode#Registers
+ */
+
+#define PATA_REGISTER(X, Y) (X + Y)
+
 enum {
-	COMMAND = 0x00,
-	STAUS	= 0x02,
-	PRDT_BASE = 0x04
+    /* IO ports */
+    PATA_PRIMARY        = 0x1F0,
+    PATA_SECONDARY      = 0x170,
+    /* Device Control Register / Alternate Status */
+    PATA_PRIMARY_DCR_AS     = 0x3F6,
+    PATA_SECONDARY_DCR_AS   = 0x376
 };
 
 enum {
-	READ_DMA_LBA28 = 0xC8,
-	READ_DMA_LBA48 = 0x25,
-	WRITE_DMA_LBA28 = 0xCA,
-	WRITE_DMA_LBA48 = 0x35
+    PATA_REG_DATA       = 0x00,
+    PATA_REG_FEATURES   = 0x01,
+    PATA_REG_ERROR      = 0x01,
+    PATA_REG_SECTOR_CNT = 0x02,
+    PATA_REG_LBA_LO     = 0x03,
+    PATA_REG_LBA_MID    = 0x04,
+    PATA_REG_LBA_HI     = 0x05,
+    PATA_REG_DRIVE      = 0x06,
+    PATA_REG_COMMAND    = 0x07,
+    PATA_REG_STATUS     = 0x07
 };
 
+enum {
+    PATA_STATUS_ERR     = 0,
+    PATA_STATUS_DRQ     = (1<<3),
+    PATA_STATUS_SRV     = (1<<4),
+    PATA_STATUS_DF      = (1<<5),
+    PATA_STATUS_RDY     = (1<<6),
+    PATA_STATUS_BSY     = (1<<7)
+};
+
+enum {
+    PATA_IDENTIFY_DRIVE     = 0,
+    PATA_IDENTIFY_FEATURES  = 83
+};
+
+enum {
+    PATA_CTRL_NIEN  = (1<<1),
+    PATA_CTRL_SRST  = (2<<1),
+    PATA_CTRL_HOB   = (7<<1)
+};
+
+enum {
+    PATA_CMD_PIO_READ           = 0x20,
+    PATA_CMD_PIO_WRITE          = 0x30,
+	PATA_CMD_READ_DMA_LBA28     = 0xC8,
+	PATA_CMD_READ_DMA_LBA48     = 0x25,
+	PATA_CMD_WRITE_DMA_LBA28    = 0xCA,
+	PATA_CMD_WRITE_DMA_LBA48    = 0x35,
+    PATA_CMD_IDENTIFY           = 0xEC
+};
+
+enum {
+    PATA_MASTER = 0xA0,
+    PATA_SLAVE  = 0xB0
+};
+
+enum {
+    IDE_PATA    = 0x00,
+    IDE_ATAPI   = 0x01
+};
 #endif
 #endif
