f116bc856566539b1ae345f33cd8045a98cdf8bf
Mateusz Kocielski <shm@digitalsun.pl>
2017-03-09 13:25:10 +0100

    What I've done :(

--- sys/dev/devtable.c
+++ sys/dev/devtable.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <sys/device.h>
 
+d_init_t    pata_init;
 d_init_t    md_init;
 d_init_t    fdc_init;
 d_init_t    null_init;
@@ -9,6 +10,7 @@ d_init_t    zero_init;
 d_init_t    *devtab[] = {
     md_init,
     fdc_init,
+    pata_init,
     null_init,
     zero_init,
     NULL
