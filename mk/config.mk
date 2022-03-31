de5056ed1285a5334ff3749e10a2420e87dc915f
Mateusz Kocielski <shm@digitalsun.pl>
2022-03-31 11:36:12 +0200

    Realign to amd64
    XXX: make it configurable in the future

--- mk/config.mk
+++ mk/config.mk
@@ -7,11 +7,13 @@ AOUT_PREFIX?=i386-aout-
 AOUT_CC=${AOUT_PREFIX}gcc
 __INCDIR= -I ${IMPALA_SRCROOT}/sys -I ${IMPALA_SRCROOT}/sys/arch/${IMPALA_ARCH}/ 
 _INCDIR?=${__INCDIR} ${INCDIR}
-__C_FLAGS=-m32 -std=c99 -ffreestanding -Wall -Wstrict-prototypes\
+# XXXamd64: ADD -m32 for x86
+__C_FLAGS=-std=c99 -ffreestanding -Wall -Wstrict-prototypes\
 	-Wmissing-prototypes ${_INCDIR} -nostdlib -D__Impala__  
-_K_FLAGS=${__C_FLAGS} -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-3dnow\
+# XXXamd64
+_K_FLAGS=-mcmodel=large -z max-page-size=0x1000 -mno-red-zone ${__C_FLAGS} -mno-mmx -mno-sse -mno-sse2 -mno-sse3 -mno-3dnow\
     -D__KERNEL -Werror -nostdinc -g -fno-inline  
-_U_FLAGS=${__C_FLAGS} ${__INCDIR} -I${IMPALA_USR}/lib/libc/include\
+_U_FLAGS=-m32 ${__C_FLAGS} ${__INCDIR} -I${IMPALA_USR}/lib/libc/include\
         -I${IMPALA_USR}/lib/libpthread/include\
         -I${IMPALA_USR}/lib/libz/ 
 _U_LDFLAGS=-Wl,-e,__start -T${IMPALA_USR}/conf/user.ld -nostdlib\
@@ -20,7 +22,8 @@ _U_LDFLAGS=-Wl,-e,__start -T${IMPALA_USR}/conf/user.ld -nostdlib\
 _CFLAGS?= ${_K_FLAGS_} ${CFLAGS}
 CC= ${PREFIX}gcc
 AS= ${PREFIX}as
-AS_FLAGS= -32 
+# XXXamd64
+# AS_FLAGS= -32 
 AR=${PREFIX}ar
 GZIP=gzip -9
 STRIP=${PREFIX}strip
@@ -28,7 +31,7 @@ AR=${PREFIX}ar
 RANLIB=${PREFIX}ranlib
 LD=${PREFIX}ld
 LIBDIR?=
-LD_FLAGS=-nostdlib -T ${LD_SCRIPT} --cref -Map ${LD_MAP} ${LIBDIR}
+LD_FLAGS=-z max-page-size=0x1000 -nostdlib -T ${LD_SCRIPT} --cref -Map ${LD_MAP} ${LIBDIR}
 INSTALL=cp
 AWK=awk
 VERBOSE?=@
